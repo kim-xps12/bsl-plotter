5ecba8393cba9b45656058574063edf4e6bc4425
aYusukeMiake <yusuke.miake@access-company.com>
2023-05-25 09:01:42 +0900

    Delete answers from lesson.py

--- catkin_ws/src/plotter_controller/src/lesson.py
+++ catkin_ws/src/plotter_controller/src/lesson.py
@@ -14,38 +14,10 @@ from arm_control import ArmControl
 
 
 def target_line(t):
-    """
-    # Lesson1
-    if(t<=7):
-        x = 20*t + 200
-    else:
-        x = 340
-    y = 50
-    # Lesson2
-    T = 5 #[sec]
-    w = 2*np.pi/T #[rad/s]
+    # Need to rewrite this place
+    x = 200
+    y = 200
 
-    x = 100*np.sin(w*t)+200 #[mm]
-    y = 50 #[mm]
-
-    """
-    #"""
-    # Lesson3
-    r = 50
-    T = 5 #[sec]
-    w = 2*np.pi/T #[rad/s]
-    x = r*np.sin(w*t)+150 #[mm]
-    y = r*np.cos(w*t)+150 #[mm]
-    #"""
-
-    """
-    #Lesson
-    T = 5
-    a = 1/100
-    w = 2*np.pi/T
-    x = 100*np.sin(w*t) + 200
-    y = a*((x - 200)**2) + 50
-    """
     return x, y
 
 
